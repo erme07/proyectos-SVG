2f0e1de2e9cf6c24152c35842e413742b94b290a
erme07 <erik.medina07@gmail.com>
2023-02-20 15:18:04 -0300

    logic for select items and custom character

--- customAmongus/script.js
+++ customAmongus/script.js
@@ -1,8 +1,29 @@
 const colores = document.querySelector(".colors");
-const characterColor = document.getElementById("characterColor");
+const hats = document.querySelector(".hats");
+const contentSVG = document.getElementById("externalSVG")
 
-colores.addEventListener('click',(e)=>{
-    if(e.target.getAttribute("data-name") === "color-selector"){
-        characterColor.style.fill = `var(--${e.target.getAttribute("data-value")})`;
-    }
-})
+contentSVG.addEventListener("load",function(){ 
+    const svgDocument = contentSVG.contentDocument;
+    const colorCharacter = svgDocument.getElementById("characterColor");
+    const hat = svgDocument.getElementById("hat");
+
+    colores.addEventListener('click',(e)=>{
+        if(e.target.getAttribute("data-name") === "color-selector"){
+            colorCharacter.style.fill = `var(--${e.target.getAttribute("data-value")})`;
+        }
+    })
+    hats.addEventListener("click", (e)=>{
+        let hatItem = "";
+        if(e.target.getAttribute("data-name") === "hat-selector"){
+            hatItem = e.target.getAttribute("data-value");
+        }
+        if(e.target.tagName === 'use'){
+            hatItem = e.target.parentElement.getAttribute("data-value");
+        }
+        if(hatItem === "hat_default"){
+            hat.innerHTML="";
+        }else{
+            hat.innerHTML = `<use href="#${hatItem}"/>`;
+        }
+    })
+});
