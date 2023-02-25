c9bd5972a465cad52eb185cdf24b1e1ab307f55b
erme07 <erik.medina07@gmail.com>
2023-02-24 22:00:56 -0300

    logica de programacion terminada

--- customAmongus/script.js
+++ customAmongus/script.js
@@ -1,84 +1,70 @@
-const colores = document.querySelector(".colors");
-const hats = document.querySelector(".hats");
-const pets = document.querySelector(".pets");
-const skins = document.querySelector(".skins")
-const contentSVG = document.getElementById("externalSVG");
-const menu = document.querySelector(".menu");
+const contentSVG = document.getElementById('externalSVG');
+const menu = document.querySelectorAll('[data-name="menu"]');
+const panels = document.querySelectorAll('[data-name="panel"]');
 const items = document.querySelector(".items");
-const options = document.querySelectorAll('[data-name="option"]');
+// const options = document.querySelectorAll('[data-name="option"]');
 
 contentSVG.addEventListener("load",function(){ 
     const svgDocument = contentSVG.contentDocument;
-    const colorCharacter = svgDocument.getElementById("characterColor");
-    const hat = svgDocument.getElementById("hat");
-    const pet = svgDocument.getElementById("pet");
-    const skin = svgDocument.getElementById("skin");
+    const colorCharacter = svgDocument.getElementById('characterColor');
 
-    colores.addEventListener('click',(e)=>{
-        if(e.target.getAttribute("data-name") === "color-selector"){
+    document.addEventListener('click',(e)=>{
+
+        if(e.target.getAttribute("data-name") === "color"){
             colorCharacter.style.fill = `var(--${e.target.getAttribute("data-value")})`;
+            const colors = document.querySelectorAll(`.${e.target.getAttribute("data-name")}s div`);
+            colors.forEach(el => {
+                el.classList.remove("selected");
+            });
+            e.target.classList.add("selected");
         }
-    })
-    hats.addEventListener("click", (e)=>{
-        let hatItem = "";
-        if(e.target.getAttribute("data-name") === "hat-selector"){
-            hatItem = e.target.getAttribute("data-value");
-            hat.innerHTML = `<use href="#${hatItem}"/>`;
+        if(e.target.getAttribute("data-name") === "hat" || e.target.getAttribute("data-name") === "skin" || e.target.getAttribute("data-name") === "pet"){
+            const elemento = svgDocument.getElementById(e.target.getAttribute("data-name"));
+            const aux = document.querySelectorAll(`.${e.target.getAttribute("data-name")}s div`);
+            aux.forEach(el => {
+                el.classList.remove("selected");
+            });
+
+            if(e.target.getAttribute("data-value") === "default"){
+                e.target.classList.add("selected");
+                elemento.innerHTML="";
+            }else{
+                e.target.parentElement.classList.add("selected");
+                elemento.innerHTML = `<use href="#${e.target.getAttribute("data-value")}"/>`;
+            }
         }
+
         if(e.target.tagName === 'use'){
-            hatItem = e.target.parentElement.getAttribute("data-value");
-            hat.innerHTML = `<use href="#${hatItem}"/>`;
+            if(e.target.parentElement.getAttribute("data-name") === "hat" || e.target.parentElement.getAttribute("data-name") === "skin" || e.target.parentElement.getAttribute("data-name") === "pet"){
+                const elementoPadre = svgDocument.getElementById(e.target.parentElement.getAttribute("data-name"));
+                aux = document.querySelectorAll(`.${e.target.parentElement.getAttribute("data-name")}s div`);
+                aux.forEach(el => {
+                    el.classList.remove("selected");
+                });
+                e.target.parentElement.parentElement.classList.add("selected");
+                elementoPadre.innerHTML = `<use href="#${e.target.parentElement.getAttribute("data-value")}"/>`;
+            }
         }
-        if(hatItem === "hat_default"){
-            hat.innerHTML="";
+
+        if(e.target.getAttribute("data-name") === 'menu'){
+            menu.forEach(el => {
+                el.classList.remove("menu__option--active");
+            });
+            e.target.classList.add("menu__option--active");
+
+            const panel = document.querySelector(`.${e.target.getAttribute("data-value")}`);
+            panels.forEach(el => {
+                el.classList.remove("active");
+            });
+            panel.classList.add("active");
+
+            if(e.target.getAttribute("data-value") === 'colors'){
+                items.classList.add("items--background");
+            }else{
+                items.classList.remove("items--background");
+            }
         }
-    })
-    pets.addEventListener("click", (e)=>{
-        let hatItem = "";
-        if(e.target.getAttribute("data-name") === "pet-selector"){
-            hatItem = e.target.getAttribute("data-value");
-            pet.innerHTML = `<use href="#${hatItem}"/>`;
-        }
-        if(e.target.tagName === 'use'){
-            hatItem = e.target.parentElement.getAttribute("data-value");
-            pet.innerHTML = `<use href="#${hatItem}"/>`;
-        }
-        if(hatItem === "pet_default"){
-            pet.innerHTML="";
-        }
-    })
-    skins.addEventListener("click", (e)=>{
-        let hatItem = "";
-        if(e.target.getAttribute("data-name") === "skin-selector"){
-            hatItem = e.target.getAttribute("data-value");
-            skin.innerHTML = `<use href="#${hatItem}"/>`;
-        }
-        if(e.target.tagName === 'use'){
-            hatItem = e.target.parentElement.getAttribute("data-value");
-            skin.innerHTML = `<use href="#${hatItem}"/>`;
-        }
-        if(hatItem === "skin_default"){
-            skin.innerHTML="";
-        }
+
     })
     
 });
-menu.addEventListener("click", (e)=>{
-    if(e.target.getAttribute("data-name") === 'option'){
-        for(i=0;i<items.childElementCount;i++){
-            items.children[i].classList.remove("active");
-        }
-        for(i=0;i<menu.childElementCount;i++){
-            menu.children[i].classList.remove("menu_active");
-        }
-        const aux = document.querySelector(`.${e.target.getAttribute("data-value")}`);
-        aux.classList.add("active");
-        e.target.classList.add("menu_active");
-        if(e.target.getAttribute("data-value") !== 'colors'){
-            items.style.backgroundColor = "unset";
-        }
-        else{
-            items.style.backgroundColor = "var(--panelSecondColor)";
-        }
-    }
-})
